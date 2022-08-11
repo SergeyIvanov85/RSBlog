b17e882bc062ac75615b3922180724ae0fa907d6
Sergei Ivanov <koldyn85@yandex.ru>
2022-08-11 16:51:42 +0300

    create: UserModel; auth/controller-route

---
+++ server/controllers/auth.js
@@ -0,0 +1,24 @@
+//Register user
+export const register = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+//Login user
+export const login = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+//Get Me
+export const getMe = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
