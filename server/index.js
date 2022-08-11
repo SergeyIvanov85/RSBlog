0816c2b4041565e4103e2801d7847822c58d4645
Sergei Ivanov <koldyn85@yandex.ru>
2022-08-11 13:37:25 +0300

    connect: mongoDB database

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+
+
 
 const app = express();
 
-app.listen(5000, () => {
-    console.log('Server started');
-})
+async function start() {
+    try {
+        await mongoose.connect(
+            'mongodb+srv://test:test123@cluster0.wddqga7.mongodb.net/rss-blog?retryWrites=true&w=majority'
+        )
+        app.listen(3002, () => console.log(`Server started on port: ${3002}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+start()
