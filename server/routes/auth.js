555a29b29c292aa79c27655bd88c6aec79951ccb
Sergei Ivanov <koldyn85@yandex.ru>
2022-08-13 12:58:13 +0300

    create: backend register, login, getMe responses

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,6 @@
 import { Router} from 'express';
 import {register, login, getMe} from "../controllers/auth.js";
+import {checkAuth} from "../utils/checkAuth.js";
 
 const router = new Router()
 
@@ -15,7 +16,7 @@ router.post('/login', login)
 
 // Get me
 //http://localhost:3002/api/auth/me
-router.get('/me', getMe)
+router.get('/me', checkAuth ,getMe)
 
 
 export default router
