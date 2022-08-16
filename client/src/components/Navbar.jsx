9a346470ddc9c52341d4233fea3e657c78cb16c5
Nina Ponomareva <nina.9412@mail.ru>
2022-08-16 19:02:49 +0300

    feat: change navbar component and styles

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React from 'react';
 
 export  const Navbar = () => {
-    return <nav className='main-nav'>
-        <ul className='main-nav__list'>
-            <li className='main-nav__link active'>Главная</li>
-            <li className='main-nav__link'>Лента</li>
-            <li className='main-nav__link'>Мои статьи</li>
-            <li className='main-nav__link'>О проекте</li>
-        </ul>
-    </nav>
+    return <header className='header-wrapper'>
+            <div className='section-wrapper header'>
+                <div className='logo'>RS BLOG</div>
+                <nav className='main-nav'>
+                    <ul className='main-nav__list'>
+                        <li className='main-nav__link active'><a href='/'>Главная</a></li>
+                        <li className='main-nav__link'><a href='/'>Лента</a></li>
+                        <li className='main-nav__link'><a href='/'>Мои статьи</a></li>
+                        <li className='main-nav__link'><a href='/'>О проекте</a></li>
+                    </ul> 
+                </nav>
+                <div className='menu'>
+                    <img src='../assets/images/menu-icon.png' alt='Menu'></img>
+                </div>
+            </div>
+    </header>
 }
