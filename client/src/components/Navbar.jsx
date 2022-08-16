269e772afc338f6b1fa9aa16c5741ab610bfe229
Sergei Ivanov <koldyn85@yandex.ru>
2022-08-16 13:20:57 +0300

    fix: variables styles

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export  const Navbar = () => {
-    return <div>Navbar</div>
+    return <nav className='main-nav'>
+        <ul className='main-nav__list'>
+            <li className='main-nav__link active'>Главная</li>
+            <li className='main-nav__link'>Лента</li>
+            <li className='main-nav__link'>Мои статьи</li>
+            <li className='main-nav__link'>О проекте</li>
+        </ul>
+    </nav>
 }
