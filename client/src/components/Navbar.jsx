d406923b61f71b88d141d91979975b26231b090f
Sergei Ivanov <koldyn85@yandex.ru>
2022-08-16 16:45:12 +0300

    add: NavLink

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { Link, NavLink} from "react-router-dom";
 
 export  const Navbar = () => {
     return <nav className='main-nav'>
         <ul className='main-nav__list'>
-            <li className='main-nav__link active'>Главная</li>
-            <li className='main-nav__link'>Лента</li>
-            <li className='main-nav__link'>Мои статьи</li>
-            <li className='main-nav__link'>О проекте</li>
+            <li className='main-nav__link active'><NavLink to={'/'} href='/'>Главная</NavLink></li>
+            <li className='main-nav__link'><NavLink to={'/posts'} href='/'>Лента</NavLink></li>
+            <li className='main-nav__link'><NavLink to={'/new'} href='/'>Мои статьи</NavLink></li>
+            <li className='main-nav__link'><a href='/'>О проекте</a></li>
         </ul>
     </nav>
 }
