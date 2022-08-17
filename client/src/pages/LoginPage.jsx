66981fdf02721ecc12af6a3e1f4f3c3c56f0dfbf
Sergei Ivanov <koldyn85@yandex.ru>
2022-08-17 13:08:42 +0300

    create: forms

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,5 +1,29 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 export const LoginPage = () => {
-    return <div>LoginPage</div>
+    return <form onSubmit={e => e.preventDefault()}
+    className=''
+    >
+        <h1 className=''>Авторизация</h1>
+        <label className=''>
+            Логин
+            <input type='text'
+            placeholder='Username'
+                   className=''
+            />
+        </label>
+
+        <label className=''>
+            Пароль
+            <input type='password'
+                   placeholder='Password'
+                   className=''
+            />
+        </label>
+        <div className=''>
+            <button type='submit' className=''>Подтвердить</button>
+            <Link to={'/register'} className=''>Еще не зарегистрированы?</Link>
+        </div>
+    </form>
 }
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,5 +1,29 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 export const RegisterPage = () => {
-    return <div>RegisterPage</div>
+    return <form onSubmit={e => e.preventDefault()}
+                 className=''
+    >
+        <h1 className=''>Регистрация</h1>
+        <label className=''>
+            Логин
+            <input type='text'
+                   placeholder='Username'
+                   className=''
+            />
+        </label>
+
+        <label className=''>
+            Пароль
+            <input type='password'
+                   placeholder='Password'
+                   className=''
+            />
+        </label>
+        <div className=''>
+            <button type='submit' className=''>Подтвердить</button>
+            <Link to={'/login'} className=''>Уже зарегистрированы?</Link>
+        </div>
+    </form>
 }
