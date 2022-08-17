deb6bb59a963db8cf884d5a83ec4d962cca438e4
Sergei Ivanov <koldyn85@yandex.ru>
2022-08-17 10:35:44 +0300

    create: buttons route

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import { Link, NavLink} from "react-router-dom";
 
 export  const Navbar = () => {
+
+    const isAuth = false;
+
     return <nav className='main-nav'>
-        <ul className='main-nav__list'>
-            <li className='main-nav__link active'><NavLink to={'/'} href='/'>Главная</NavLink></li>
-            <li className='main-nav__link'><NavLink to={'/posts'} href='/'>Лента</NavLink></li>
-            <li className='main-nav__link'><NavLink to={'/new'} href='/'>Мои статьи</NavLink></li>
-            <li className='main-nav__link'><NavLink to={'/about'} href='/'>О проекте</NavLink></li>
-        </ul>
+        {
+            isAuth && (
+                <ul className='main-nav__list'>
+                    <li className='main-nav__link active'><NavLink to={'/'} href='/'>Главная</NavLink></li>
+                    <li className='main-nav__link'><NavLink to={'/posts'} href='/'>Лента</NavLink></li>
+                    <li className='main-nav__link'><NavLink to={'/new'} href='/'>Мои статьи</NavLink></li>
+                    <li className='main-nav__link'><NavLink to={'/about'} href='/'>О проекте</NavLink></li>
+                </ul>
+            )
+        }
     </nav>
 }
