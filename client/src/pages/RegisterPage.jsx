30fd1941ebabd1e5532e86502667ef7e242890a6
Nina Ponomareva <nina.9412@mail.ru>
2022-08-17 16:05:04 +0300

    feat: add LoginPage, RegisterPage (layout and styles)

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,5 +1,24 @@
-import React from "react";
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 export const RegisterPage = () => {
-    return <div>RegisterPage</div>
+    return <form onSubmit={e => e.preventDefault()} className='section-wrapper form-container'>
+        <div className='form-inner'>
+            <h1 className='auth-form-title'>Регистрация</h1>
+            <label className='label'>Логин:
+                <input type='text'
+                    placeholder='Username'
+                    className='input'/>
+            </label>
+            <label className='label'>Пароль:
+                <input type='password'
+                    placeholder='Password'
+                    className='input'/>
+            </label>
+            <div className='confirm-btns'>
+                <button type='submit' className='btn'>Подтвердить</button>
+                <Link to={'/login'} className='login-link'>Уже зарегистрированы?</Link>
+            </div>
+        </div>
+    </form>
 }
