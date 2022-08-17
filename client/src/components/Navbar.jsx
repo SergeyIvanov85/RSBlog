5987f441ded241806a53fe5571fd95bfda7a6cdc
Nina Ponomareva <nina.9412@mail.ru>
2022-08-17 12:55:37 +0300

    fix: fix navigation links behavior

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
-import menu from '../assets/images/menu-icon.png'
+import menu from '../assets/images/menu-icon.png';
+import { NavLink } from "react-router-dom";
 
 export  const Navbar = () => {
+
+    const isAuth = true;
+
     return <header className='header-wrapper'>
             <div className='section-wrapper header'>
                 <div className='logo'>RS BLOG</div>
                 <nav className='main-nav'>
-                    <ul className='main-nav__list'>
-                        <li className='main-nav__link active'><a href='/'>Главная</a></li>
-                        <li className='main-nav__link'><a href='/'>Лента</a></li>
-                        <li className='main-nav__link'><a href='/'>Мои статьи</a></li>
-                        <li className='main-nav__link'><a href='/'>О проекте</a></li>
-                    </ul> 
+                    { isAuth && (
+                        <ul className='main-nav__list'>
+                            <li className='main-nav__link'><NavLink to={'/'}><a href='/'>Главная</a></NavLink></li>
+                            <li className='main-nav__link'><NavLink to={'/posts'}><a href='/'>Лента</a></NavLink></li>
+                            <li className='main-nav__link'><NavLink to={'/new'}><a href='/'>Мои статьи</a></NavLink></li>
+                            <li className='main-nav__link'><NavLink to={'/about'}><a href='/'>О проекте</a></NavLink></li>
+                        </ul> 
+                    )}
                 </nav>
                 <div className='menu'>
-                    <img src={menu} alt='Menu'></img>
+                    { isAuth && (
+                        <img src={menu} alt='Menu'></img>
+                    )}
                 </div>
             </div>
     </header>
