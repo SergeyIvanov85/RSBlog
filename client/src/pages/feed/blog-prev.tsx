f5af33ca7da3387fef2894a43e91f5632a7691a7
Sweta <Stellla@tut.by>
2022-08-19 23:03:23 +0200

    fix: fixed styles

--- client/src/pages/feed/blog-prev.tsx
+++ client/src/pages/feed/blog-prev.tsx
@@ -1,26 +1,20 @@
-import { BlogInfo } from "./blog-info"
-import { Count } from "./count"
-import { IBlogItem } from "./interfaces"
+import { BlogInfo } from "./blog-info";
+import { Count } from "./count";
+import { IBlogItem } from "./interfaces";
 
-interface BlogItemProps{
-  data:IBlogItem
+interface BlogItemProps {
+  data: IBlogItem;
 }
 
-export function BlogPrev ({data}:BlogItemProps){
-
+export function BlogPrev({ data }: BlogItemProps) {
   return (
-    <div className="blog">
-           <BlogInfo info={data}/>
-           <div className="blog__text_wrapper">
-           <div className="blog__text">
-                   {data.text}
-           </div>
-           </div>
-           <div className="blog__count">
-           <Count count={data.count}/>
-           <button> Подробнее </button>
-           </div>
-          
-     </div>
-  )
+    <div className='blog'>
+      <BlogInfo info={data} />
+      <div className='blog__text'>{data.text}</div>
+      <div className='blog__count'>
+        <Count count={data.count} />
+        <button> Подробнее </button>
+      </div>
+    </div>
+  );
 }
