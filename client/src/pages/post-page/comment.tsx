a547247365ba976606a5ac1382190602bc2d1a34
Sweta <Stellla@tut.by>
2022-08-20 22:54:37 +0200

    feat: add block comments in post page

--- client/src/pages/post-page/comment.tsx
+++ client/src/pages/post-page/comment.tsx
@@ -1,3 +1,24 @@
+import { CommentItem } from "./comments-item";
+
 export function Comments() {
-  return <div className='comments__wrapper'>Comments</div>;
+  return (
+    <div className='comments'>
+      <div className='comments__button-close'></div>
+      <div className='comments__list'>
+        <CommentItem />
+        <CommentItem />
+        <CommentItem />
+        <CommentItem />
+        <CommentItem />
+        <CommentItem />
+        <CommentItem />
+        <CommentItem />
+      </div>
+      <textarea placeholder='Комментировать'></textarea>
+      <div className='comments__buttons'>
+        <button className='btn btn_small'>Удалить</button>
+        <button className='btn btn_small'>Сoхранить</button>
+      </div>
+    </div>
+  );
 }
---
+++ client/src/pages/post-page/comments-item.tsx
@@ -0,0 +1,8 @@
+export function CommentItem() {
+  return (
+    <div className='comment-item'>
+      <div className='comment-item__user'>SI</div>
+      <div className='comment-item__text'>Test comment</div>
+    </div>
+  );
+}
