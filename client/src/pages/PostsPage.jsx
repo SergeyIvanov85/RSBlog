d5378fca76f2f21fdfd9aca05c590df17f8ca883
Nina Ponomareva <nina.9412@mail.ru>
2022-08-24 19:01:38 +0300

    feat: create basic layout and stylesheet for PostsPage, delete excess styles in add_post_page.scss

--- client/src/pages/PostsPage.jsx
+++ client/src/pages/PostsPage.jsx
@@ -1,5 +1,20 @@
 import React from "react";
+import {PostItem} from "../components/PostItem";
+import {useDispatch, useSelector} from "react-redux";
 
 export const PostsPage = () => {
-    return <div>PostsPage</div>
+
+    const {posts} = useSelector((state) => state.post)
+
+    if(!posts.length) {
+      return (
+        <div className='no-posts'>Кажется, вы еще не добавили ни одной статьи &#129300;</div>
+      )
+    }
+
+    return <div className='section-wrapper'>
+        <div className='user-posts'>
+            {posts?.map((post, idx) => (<PostItem key={idx} post={post}/>))}
+        </div>
+    </div>
 }
