45e316915f34efa0b49bd155c53909597ab9edba
Nina Ponomareva <nina.9412@mail.ru>
2022-08-28 19:01:27 +0300

    refactor: change extra-reducers syntax

--- client/src/redux/features/post/postSlice.ts
+++ client/src/redux/features/post/postSlice.ts
@@ -1,42 +1,110 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+import { IPost } from "../../../models";
 
 const initialState = {
   posts: [],
   popularPosts: [],
+  topicPosts: [],
   loading: false,
 };
 
 export const createPost = createAsyncThunk(
-  "post/createPost",
+  'post/createPost', 
   async (params) => {
-    try {
-      const { data } = await axios.post("/posts", params);
-      return data;
-    } catch (error) {
-      console.log(error);
-    }
+  try {
+    const {data} = await axios.post('/posts', params)
+    return data
+  } catch (error) {
+    console.log(error)
   }
-);
-/*
+})
+
+export const getAllPosts = createAsyncThunk(
+  'post/getAllPosts', async () => {
+  try {
+    const {data} = await axios.get('/posts')
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+export const removePost = createAsyncThunk(
+  'post/removePost', async (id) => {
+  try {
+    const {data} = await axios.delete(`/posts/${id}`, id)
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+export const updatePost = createAsyncThunk(
+  'post/updatePost', async (updatedPost) => {
+  try {
+    const {data} = await axios.put(`/posts/${updatedPost.id}`, updatedPost)
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+})
+
 export const postSlice = createSlice({
-  name: "post",
+  name: 'post',
   initialState,
   reducers: {},
-  extraReducers: {
+  extraReducers: (builder) => {
+    builder
     // Create Post
-    [createPost.pending]: (state) => {
-      state.loading = true;
-    },
-    [createPost.fulfilled]: (state, action) => {
-      state.loading = false;
-      state.posts.push(action.payload);
-    },
-    [createPost.rejected]: (state) => {
-      state.loading = false;
-    },
-  },
-});
+    .addCase(createPost.pending, (state) => {
+      state.loading = true
+    })
+    .addCase(createPost.fulfilled, (state, action) => {
+      state.loading = false
+      state.posts.push(action.payload)
+    })
+   .addCase(createPost.rejected, (state) => {
+      state.loading = false
+    })
 
-export default postSlice.reducer;
-*/
+    // Getting All Posts
+   .addCase(getAllPosts.pending, (state) => {
+      state.loading = true
+    })
+   .addCase(getAllPosts.fulfilled, (state, action) => {
+      state.loading = false
+      state.posts = action.payload.posts
+      state.popularPosts = action.payload.popularPosts
+      state.topicPosts = action.payload.topicPosts
+    })
+   .addCase(getAllPosts.rejected, (state) => {
+      state.loading = false
+    })
+    // Delete Post
+   .addCase(removePost.pending, (state) => {
+      state.loading = true
+    })
+   .addCase(removePost.fulfilled, (state, action) => {
+      state.loading = false
+      state.posts = state.posts.filter((post) => post._id !== action.payload._id)
+    })
+   .addCase(removePost.rejected, (state) => {
+      state.loading = false
+    })
+    // Update Post
+   .addCase(updatePost.pending, (state) => {
+      state.loading = true
+    })
+   .addCase(updatePost.fulfilled, (state, action) => {
+      state.loading = false
+      const index = state.posts.findIndex((post) => post._id === action.payload._id)
+      state.posts[index] = action.payload
+    })
+   .addCase(updatePost.rejected, (state) => {
+      state.loading = false
+    })
+  },
+})
+
+export default postSlice.reducer
