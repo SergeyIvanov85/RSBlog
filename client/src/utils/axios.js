05816e22c9f5741f4d8fa613346a0e4b263079a1
Nina Ponomareva <nina.9412@mail.ru>
2022-08-29 18:58:33 +0300

    feat: add utils, axios

---
+++ client/src/utils/axios.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    baseURL: 'http://localhost:3002/api',
+    validateStatus: () => true,
+})
+
+instance.interceptors.request.use(config => {
+    config.headers.Authorization = window.localStorage.getItem('token')
+
+    return config
+})
+
+export default instance
