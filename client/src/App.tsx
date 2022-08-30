953732334b093f3e557b3d25421b545a44eac950
Sweta <Stellla@tut.by>
2022-08-30 23:38:56 +0200

    fix: migration to TS  FeedPostsPage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,39 +1,41 @@
 //import { AddPostPage } from "./pages/AddPostPage";
-import {useEffect} from "react";
+import { useEffect } from "react";
 import { useAppDispatch } from "./redux/hooks";
 import { getMe } from "./redux/features/auth/authSlice";
-import {ToastContainer} from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import {Routes, Route} from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Routes, Route } from "react-router-dom";
 
-import {Layout} from "./components/Layout";
-import {MainPage} from "./pages/MainPage";
+import { Layout } from "./components/Layout";
+import { MainPage } from "./pages/MainPage";
 import { PostPage } from "./pages/PostPage";
-import {RegisterPage} from "./pages/RegisterPage";
-import {LoginPage} from "./pages/LoginPage";
-import {AddPostPage} from "./pages/AddPostPage";
-import {PostsPage} from "./pages/PostsPage";
+import { RegisterPage } from "./pages/RegisterPage";
+import { LoginPage } from "./pages/LoginPage";
+import { AddPostPage } from "./pages/AddPostPage";
+import { PostsPage } from "./pages/PostsPage";
+import { FeedPostsPage } from "./pages/FeedPostsPage";
 
 function App() {
-  const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
-      dispatch(getMe())
-  }, [dispatch])
+    dispatch(getMe());
+  }, [dispatch]);
 
-return (
-  <Layout>
-    <Routes>
-    <Route path='login' element={<LoginPage />} />
-    <Route path='/' element={<MainPage />} />
-    <Route path=':id' element={<PostPage />} />
-    <Route path='register' element={<RegisterPage />} />
-    <Route path='new' element={<AddPostPage />} />
-    <Route path='posts' element={<PostsPage />} />
-    </Routes>
-    <ToastContainer position='bottom-right' />
-  </Layout>
-);
+  return (
+    <Layout>
+      <Routes>
+        <Route path='login' element={<LoginPage />} />
+        <Route path='/' element={<MainPage />} />
+        <Route path=':id' element={<PostPage />} />
+        <Route path='register' element={<RegisterPage />} />
+        <Route path='new' element={<AddPostPage />} />
+        <Route path='feed' element={<FeedPostsPage />} />
+        <Route path='posts' element={<PostsPage />} />
+      </Routes>
+      <ToastContainer position='bottom-right' />
+    </Layout>
+  );
 }
 
 export default App;
