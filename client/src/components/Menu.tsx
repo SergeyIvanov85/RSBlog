70745e3d86cd0170a449fc77ecc7d95882e7ba6d
Nina Ponomareva <nina.9412@mail.ru>
2022-09-01 16:29:47 +0300

    feat: update styles for menu (change positioning conserning different sizes of screens)

--- client/src/components/Menu.tsx
+++ client/src/components/Menu.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
-import { logout } from "../redux/features/auth/authSlice"
-import { useAppDispatch } from "../redux/hooks"
-import {toast} from "react-toastify";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { logout } from '../redux/features/auth/authSlice'
+import { useAppDispatch } from '../redux/hooks'
+import {toast} from 'react-toastify';
+import { Link } from 'react-router-dom';
 
 const iconImg = require('../assets/images/menu-icon.png');
 
@@ -17,31 +17,34 @@ export const Menu = () => {
         toast('Вы вышли из аккаунта')
     } 
 
-    return ( 
-        <>
-        <div className={active ? 'menu-icon menu-icon_active' : 'menu-icon'} onClick={() => setActive(!active)}>
+    const toggleMenu = () => {
+        setActive(!active);
+    }
+
+    return (
+        <> 
+        <div className={active ? 'menu-icon menu-icon_active' : 'menu-icon'} onClick={() => toggleMenu()}>
             <img src={iconImg} alt='Menu'></img>
-        </div>
-                
-        <div className={active ? 'menu menu_active' : 'menu'} onClick={() => setActive(false)}>
-                <div className="disabled"></div>
-                <div className="menu-content" onClick={(e) => e.stopPropagation()}>
-                    <ul className="menu-content__list">
-                        <li className="menu-content__item">
-                            <p>Сменить тему</p>
-                        </li>
-                        <li className="menu-content__item">
-                            <p>Switch to English</p>
-                        </li>
-                        <li className="menu-content__item">
-                            <p>Статистика</p>
-                        </li>
-                        <li className="menu-content__item">
-                            <button onClick={logoutHandler} className='btn login-btn'><Link to={"/"}>Выйти</Link></button>
-                        </li>
-                    </ul>
-                </div>
+        </div>            
+        <div className={active ? 'disabled disabled_active' : 'disabled'} onClick={() => toggleMenu()}></div>
+        <div className={active ? 'menu menu_active' : 'menu'} onClick={() => toggleMenu()}>    
+            <div className='menu-content' onClick={(e) => e.stopPropagation()}>
+                <ul className='menu-content__list'>
+                    <li className='menu-content__item'>
+                        <p>Сменить тему</p>
+                    </li>
+                    <li className='menu-content__item'>
+                        <p>Switch to English</p>
+                    </li>
+                    <li className='menu-content__item'>
+                        <p>Статистика</p>
+                    </li>
+                    <li className='menu-content__item'>
+                        <button onClick={logoutHandler} className='btn login-btn'><Link to={'/'}>Выйти</Link></button>
+                    </li>
+                </ul>
             </div>
+        </div>
         </>
     )
 }
