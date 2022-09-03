d8839e4368e1b12aaeaaf63c250c7e6de3c43b36
Sweta <Stellla@tut.by>
2022-09-03 17:46:45 +0200

    fix: delete unused folders

--- client/src/pages/post-page/comments-item.tsx
+++
@@ -1,8 +0,0 @@
-export function CommentItem() {
-  return (
-    <div className='comment-item'>
-      <div className='comment-item__user'>SI</div>
-      <div className='comment-item__text'>Test comment</div>
-    </div>
-  );
-}
