48b6bf1a57cda9a1b50332133993221379b68335
Nina Ponomareva <nina.9412@mail.ru>
2022-09-04 22:57:58 +0300

    feat: add translation for EditPostPage

--- client/src/pages/EditPostPage.tsx
+++ client/src/pages/EditPostPage.tsx
@@ -3,9 +3,10 @@ import {useAppDispatch} from "../redux/hooks";
 import {useNavigate, useParams} from "react-router-dom";
 import axios from "../utils/axios";
 import {updatePost} from "../redux/features/post/postSlice";
+import { useTranslation } from 'react-i18next';
 
 export const EditPostPage = () => {
-
+    const { t } = useTranslation();
     const [title, setTitle] = useState('')
     const [text, setText] = useState('')
     const [oldImage, setOldImage] = useState<File | null>(null)
@@ -61,7 +62,7 @@ export const EditPostPage = () => {
                     setOldImage(null)
                     }
                 }}/>
-                <label htmlFor='add-image__input' className='add-image__label'>Прикрепить изображение:</label>
+                <label htmlFor='add-image__input' className='add-image__label'>{t('add-post-page.add-image')}</label>
                 <div className='image-preview'>
                     { oldImage && (
                       <img src={`http://localhost:3002/${oldImage}`} alt={oldImage.name}/>
@@ -73,33 +74,33 @@ export const EditPostPage = () => {
             </div>
 
             <div className='add-title'>
-                <label htmlFor='add-title__input'>Заголовок статьи:</label>
-                <input type='text' id='add-title__input' className='text-input'value={title} onChange={(e) => setTitle(e.target.value)} placeholder='Заголовок'/>
+                <label htmlFor='add-title__input'>{t('add-post-page.title')}</label>
+                <input type='text' id='add-title__input' className='text-input'value={title} onChange={(e) => setTitle(e.target.value)} placeholder={t('add-post-page.title-placeholder')}/>
             </div>
 
 
             <div className='add-text'>
-                <label htmlFor='add-text__input'>Текст статьи:
-                    <textarea id='add-text__input' onChange={(e) => setText(e.target.value)} value={text} placeholder='Введите текст...' rows={7} className='text-input'/>
+                <label htmlFor='add-text__input'>{t('add-post-page.text')}
+                    <textarea id='add-text__input' onChange={(e) => setText(e.target.value)} value={text} placeholder={t('add-post-page.text-placeholder')} rows={7} className='text-input'/>
                 </label>
             </div>
 
             <div className='add-topic'>
-                <label htmlFor='add-topic__select'>Тема статьи:
+                <label htmlFor='add-topic__select'>{t('add-post-page.topic')}
                     <select name='add-topic__select' onChange={(e) => setTopic(e.target.value)} value={topic} id='add-topic__select' className='add-topic__select'>
-                        <option>Выбрать тему:</option>
-                        <option value='travel'>Путешествия</option>
-                        <option value='health'>Здоровье</option>
-                        <option value='education'>Образование</option>
-                        <option value='sport'>Спорт</option>
-                        <option value='other'>Другое</option>
+                        <option value='' disabled>{t('add-post-page.topic-placeholder')}</option>
+                        <option value='travel'>{t('add-post-page.traveling')}</option>
+                        <option value='health'>{t('add-post-page.health')}</option>
+                        <option value='education'>{t('add-post-page.education')}</option>
+                        <option value='sport'>{t('add-post-page.sport')}</option>
+                        <option value='other'>{t('add-post-page.other')}</option>
                     </select>
                 </label>
             </div>
 
             <div className='add-post-btns'>
-                <button onClick={submitHandler} className='btn'>Редактировать</button>
-                <button onClick={clearFormHandler} className='btn'>Отменить</button>
+                <button onClick={submitHandler} className='btn'>{t('add-post-page.add')}</button>
+                <button onClick={clearFormHandler} className='btn'>{t('add-post-page.cancel')}</button>
             </div>
         </div>
     </form>
