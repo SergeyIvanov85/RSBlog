9e26a6d4c1782b3543633113fac8d34029861787
Nina Ponomareva <nina.9412@mail.ru>
2022-09-05 17:49:33 +0300

    feat: add AboutPage and styles

--- client/src/pages/AboutPage.tsx
+++ client/src/pages/AboutPage.tsx
@@ -1,7 +1,65 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+const ghIcon = require('../assets/images/github-icon.png');
+const sergeyAvatar = require('../assets/images/sergey-avatar.jpg');
+const svetlanaAvatar = require('../assets/images/sveta-avatar.png');
+const ninaAvatar = require('../assets/images/nina-avatar.jpg');
 
 export const AboutPage = () => {
+    const { t } = useTranslation();
+
     return (
-        <div>AboutPage</div>
+        <div className='section-wrapper about'>
+            <div className='about__goals'>
+                <h2>Цель проекта</h2>
+                <p>Привет! Наш блог - это пространство для творчества и общения. Тут вы можете делиться своими мыслями, путешествиями, новостями, историями, находить единомышленников и интересных людей.</p>
+            </div>
+            
+            <div className='about__team'>
+                <h2>Наша команда</h2>
+                <div className='team-members-grid'>
+                    <div className='team-member'>
+                        <div className='team-member__avatar sergey'>
+                            <img src={sergeyAvatar} alt="Sergey" />
+                        </div>
+                        <h3 className='team-member__role'><span>Сергей</span>Team lead<br/>Back-end Developer</h3>
+                        <p className='team-member__description'></p>
+                        <div className='team-member__git'>
+                            <a href='https://github.com/SergeyIvanov85' className='github-link'>
+                                <img src={ghIcon} alt='Github:'></img>
+                                <p>SergeyIvanov85</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div className='team-member'>
+                    <div className='team-member__avatar nina'>
+                    <img src={ninaAvatar} alt="Nina" />
+                    </div>
+                        <h3 className='team-member__role'><span>Нина</span>Front-End Developer</h3>
+                        <p className='team-member__description'></p>
+                        <div className='team-member__git'>
+                            <a href='https://github.com/Ponomareva-Nina' className='github-link'>
+                                <img src={ghIcon} alt='Github:'></img>
+                                <p>Ponomareva-Nina</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div className='team-member'>
+                    <div className='team-member__avatar svetlana'>
+                        <img src={svetlanaAvatar} alt="Svetlana" />
+                    </div>
+                        <h3 className='team-member__role'><span>Cветлана</span>Front-End Developer</h3>
+                        <p className='team-member__description'></p>
+                        <div className='team-member__git'>
+                            <a href='https://github.com/Swettlana' className='github-link'>
+                                <img src={ghIcon} alt='Github:'></img>
+                                <p>Swettlana</p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
