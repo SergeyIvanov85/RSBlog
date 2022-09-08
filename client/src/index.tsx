7a8a401580ea50815df96dfad7bc42c1929eb44e
Nina Ponomareva <nina.9412@mail.ru>
2022-09-08 18:16:34 +0300

    fix: replace BrowserRouter for HashRouter

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import {store} from "./redux/store";
 import './translation/i18n';
@@ -11,9 +11,9 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-    <BrowserRouter basename="/RSBlog">
+    <HashRouter basename="/RSBlog">
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
 );
